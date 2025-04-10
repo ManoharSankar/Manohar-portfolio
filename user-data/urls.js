353e110d206a57df81b1e326ff4d7ba31d919b16
ManoharSankar <manoharsankar93@gmail.com>
2025-04-10 07:46:15 +0530

    customized the code

--- user-data/urls.js
+++ user-data/urls.js
@@ -1,7 +1,10 @@
 const githubUsername = "ManoharSankar";
 
 const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
+const gitRepos = (username) => `https://pinned.berrysauce.dev/get/${username}`;
 
 export const URLs = {
     gitConnected: createGitConnectedURL(githubUsername),
+    gitRepo: gitRepos(githubUsername),
+
 };
