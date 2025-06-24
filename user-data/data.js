7c785dabf39729f995bfb77245cf7578fea74143
ManoharSankar <manoharsankar93@gmail.com>
2025-06-24 12:21:31 +0530

    modified code

--- user-data/data.js
+++ user-data/data.js
@@ -12,7 +12,7 @@ export const skills = [
 export const projects = [
   {
     title: "CI/CD Pipeline for Dockerized React App",
-    techStack: "React.js, Docker, Jenkins, GitHub Actions, AWS, Prometheus, Grafana",
+    tags: [ "React.js, Docker, Jenkins, GitHub Actions, AWS, Prometheus, Grafana" ],
     description:
       "Built a CI/CD pipeline that pushes Docker images to dev and prod repos based on branch. Integrated Prometheus and Grafana for health monitoring.",
     link: "https://github.com/ManoharSankar/ReactappCapstoneProject.git",
@@ -20,7 +20,7 @@ export const projects = [
   },
     {
     title: "AWS Infrastructure with Terraform",
-    techStack: "Terraform, AWS EC2, S3, IAM, GitHub Actions",
+    tags: [ "Terraform", "AWS","EC2", "S3", "IAM", "GitHub Actions" ],
     description:
       "Provisioned AWS infra with Terraform and automated deployments using GitHub Actions.",
     github: "https://github.com/ManoharSankar/TerraformEC2Webserver.git",
