db3c80e134f9704737f845e80147488635eb4d19
ManoharSankar <manoharsankar93@gmail.com>
2025-06-24 10:15:49 +0530

    Update data.js

--- user-data/data.js
+++ user-data/data.js
@@ -11,6 +11,23 @@ export const skills = [
 
 export const experience = [
   {
+    title: "CI/CD Pipeline for Dockerized React App",
+    techStack: "React.js, Docker, Jenkins, GitHub Actions, AWS, Prometheus, Grafana",
+    description:
+      "Built a CI/CD pipeline that pushes Docker images to dev and prod repos based on branch. Integrated Prometheus and Grafana for health monitoring.",
+    github: "https://github.com/ManoharSankar/ReactappCapstoneProject.git",
+  },
+  {
+    title: "AWS Infrastructure with Terraform",
+    techStack: "Terraform, AWS EC2, S3, IAM, GitHub Actions",
+    description:
+      "Provisioned AWS infra with Terraform and automated deployments using GitHub Actions.",
+    github: "https://github.com/ManoharSankar/TerraformEC2Webserver.git",
+    github: "https://github.com/ManoharSankar/Terraforms3bucket.git" 
+  },
+
+  
+  {
     title: "Algonomy Software",
     duration: "January 2019 - June 2019",
     subtitle: "DevOps Engineer",
